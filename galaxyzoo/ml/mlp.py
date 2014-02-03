c3344e71bad19e5873df997d01707e49a5071290
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2014-02-03 22:02:28 +0530

    Better Perceptron algorithm

--- galaxyzoo/ml/mlp.py
+++ galaxyzoo/ml/mlp.py
@@ -1,6 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
-
 
 X = np.array([[0, 0],
               [0, 1],
@@ -22,7 +20,6 @@ def simulate_2d_gaussian_clusters():
     return x
 
 
-
 class Perceptron(object):
 
     def __init__(self, alpha=0.3, theta=0.2, maxiter=1000):
@@ -30,17 +27,19 @@ class Perceptron(object):
         self.theta = theta
         self.maxiter = maxiter
 
-    def fit(self, x, y, weights=None, bias=None):
+    def fit(self, x, y, learning_steps=True, weights=None, bias=None):
 
         if weights is None:
-            self.weights = np.random.random((x.shape[1],))
+            self.org_weights = np.random.random((x.shape[1],))
+            self.weights = self.org_weights.copy()
         elif weights.ndim > 1:
             self.weights = weights.ravel()
         else:
             self.weights = weights
 
         if bias is None:
-            self.bias = np.random.random()
+            self.org_bias = np.random.random()
+            self.bias = self.org_bias
 
         self.x = x
 
@@ -49,21 +48,35 @@ class Perceptron(object):
         else:
             self.y = y
 
-        prediction = self.predict(self.x)
+        prediction = self.evaluate(self.x)
         n_iter = 0
+
+        steps = (50, 100, 500)
+        learning = [(self.org_weights[0], self.org_weights[1],
+                     self.org_bias)]
+
         while not np.allclose(prediction, self.y):
             addition = self.alpha * np.sum(self.y * self.x.T, axis=1)
             self.weights += addition
             self.bias += self.alpha * np.sum(self.y)
-            prediction = self.predict(self.x)
+            prediction = self.evaluate(self.x)
             n_iter += 1
             #if n_iter % 100 == 0:
             #    print n_iter
+            if n_iter in steps:
+                learning.append((self.weights[0], self.weights[1], self.bias))
             print n_iter
             if n_iter == self.maxiter:
                 break
 
-    def predict(self, x):
+        learning.append((self.weights[0], self.weights[1], self.bias))
+        if learning_steps:
+            return learning, n_iter
+
+
+
+
+    def evaluate(self, x):
         y_in = np.dot(self.x, self.weights) + self.bias
         y = np.zeros(self.y.shape)
         try:
@@ -71,6 +84,12 @@ class Perceptron(object):
             y[y_in < -self.theta] = -1
         except ValueError:
             from IPython.core.debugger import Tracer;Tracer()()
+        return y
+
+    def predict(self, x):
+        y = np.dot(x, self.weights) + self.bias
+        y[y > self.theta] = 1
+        y[y < self.theta] = -1
         return y
 
 
@@ -91,12 +110,12 @@ class MultiLayerPerceptron(object):
 
 if __name__ == "__main__":
     x = simulate_2d_gaussian_clusters()
-#    trainx, trainy = x[:700,:2], x[700:, 2]
-#    print trainx.shape, trainy.shape
-#    testx, testy = x[:700,:2], x[700:, 2]
-#    perc = Perceptron(maxiter=10000)
-#    perc.fit(trainx, trainy)
-#    pred = perc.predict(testx)
-#    print np.allclose(pred, testy.ravel())
-
+    x = x - x.mean(0)
+    x = x/x.std(0)
+    trainx, trainy = x[:700,:2], x[:700,2]
+    testx, testy = x[700:,:2], x[700:,2]
+    perc = Perceptron(theta=0, maxiter=10000)
+    perc.fit(trainx, trainy)
+    pred = perc.predict(testx)
+    print np.allclose(pred, testy)
 
--- galaxyzoo/tests/test_ml.py
+++ galaxyzoo/tests/test_ml.py
@@ -1,31 +1,46 @@
 import unittest
-from galaxyzoo.ml.mlp import MultiLayerPerceptron, Perceptron
+from nose import SkipTest
+
 import numpy as np
 
+from galaxyzoo.ml.mlp import Perceptron
+from sklearn.datasets import load_iris
+from sklearn.decomposition import PCA
 
-class TestML(unittest.TestCase):
 
-    def setUp(self):
-        self.x = np.array([[0, 0],
-                           [0, 1],
-                           [1, 0],
-                           [1, 1]], dtype=np.float)
+class TestPerceptron(unittest.TestCase):
 
-    def test_perceptron_and(self):
-        percept = Perceptron()
-        y = np.zeros((4,), dtype=np.float)
-        y[3] = 1
-        percept.fit(self.x, y)
-        y_hat = percept.predict(self.x)
-        self.assertTrue(np.allclose(y_hat, y))
+    @classmethod
+    def setUpClass(cls):
+        iris_data = load_iris()
+        data = iris_data['data']
+        data = PCA(2).fit_transform(data)
+        data = np.concatenate((data, iris_data['target'].reshape((150,1))),
+                              axis=1)
+        np.random.shuffle(data)
+        data[data == 0] = -1
+        cls.data = data
+        cls.classifier = Perceptron(theta=0)
 
-    def test_backprop_xor(self):
-        """
-        Test if backpropagation works for the XOR gate
-        :return:
-        """
-        mlp = MultiLayerPerceptron()
-        y = np.array([0,1,1,0])
-        mlp.fit(self.x, y)
-        y_hat = mlp.predict(self.x)
-        self.assertTrue(np.allclose(y_hat, y))
+    def test_classify_setosa_vs_virginica(self):
+        raise SkipTest()
+        data = self.data[self.data[:,2] != 2, :]
+        trainx = data[:50,:2]
+        trainy = data[:50,2]
+        testx = data[50:,:2]
+        testy = data[50:,2]
+        self.classifier.fit(trainx, trainy.ravel())
+        prediction = self.classifier.predict(testx)
+        self.assertTrue(np.allclose(prediction, testy.ravel()))
+
+    def test_linear_inseparable_classes(self):
+        x = np.random.multivariate_normal([0, 0], [[1, 0],[0, 1]], 1000)
+        targets = np.ones((1000,))
+        targets[:500] = -1
+        np.random.shuffle(targets)
+        learning, n_iter = self.classifier.fit(x, targets)
+        self.assertEqual(n_iter, 1000)
+
+
+if __name__ == "__main__":
+    unittest.main()
